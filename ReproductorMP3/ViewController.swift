9e567b95f31c421aaf3068c9be121c14a52502fe
Carlos Buitrago <Carlos@macbook-pro-de-carlos.local>
2016-04-10 13:34:53 -0500

    1

--- ReproductorMP3/ViewController.swift
+++ ReproductorMP3/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDelegate,UITextFieldDelegate {
+    
+    let arr: [String] = ["- - - -","Desde el Día en Que Te Fuiste","Tú Tienes Razón ft. Silvestre Dangond","What A Wonderful World"]
+    var flag = false
+    var numero  = 0
+    private var reproductor: AVAudioPlayer!
+    
+    @IBOutlet weak var caratula: UIImageView!
+    @IBOutlet weak var titulo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        titulo.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,76 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        numero = arr.count-1
+        return arr.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arr[row]
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (arr[row] != "- - - -"){
+            reproducir(arr[row])
+        }
+        else{
+            titulo.text = ""
+            if flag{
+                if reproductor.playing{
+                    reproductor.stop()
+                    reproductor.currentTime = 0.0
+                }
+            }
+            flag = false
+            caratula.image = UIImage(named: "Blanco.jpg")
+        }
+    }
+    func reproducir(dato: String){
+        flag = true
+        let sonidoURL = NSBundle.mainBundle().URLForResource(dato, withExtension: "mp3")
+        do{
+            try reproductor = AVAudioPlayer(contentsOfURL: sonidoURL!)
+        }
+        catch{
+            print("Error cargar archivo de sonido")
+        }
+        reproductor.play()
+        titulo.text = dato
+        caratula.image = UIImage(named: dato+".jpg")
+    }
+    @IBAction func Tocar() {
+        if flag{
+            if !reproductor.playing{
+                reproductor.play()
+            }
+        }
+    }
+    @IBAction func Pausar() {
+        if flag{
+            if  reproductor.playing{
+                reproductor.pause()
+            }
+        }
+    }
+    @IBAction func Detener() {
+        if flag{
+            if reproductor.playing{
+                reproductor.stop()
+                reproductor.currentTime = 0.0
+            }
+        }
+    }
+    @IBAction func volumen(sender: UISlider) {
+        reproductor.volume = sender.value
+    }
+    @IBAction func shuffle() {
+        let randon = Int(arc4random_uniform(UInt32(numero)) + 1)
+        reproducir(arr[randon])
+        titulo.text = String(randon)
+        flag = true
+    }
 
 }
 
